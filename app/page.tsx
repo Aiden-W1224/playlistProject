7e68de1f5c66f0711ec737b0d21d8e80f4561868
Amka Tsendjargal <amka150@gmail.com>
2023-11-10 15:01:53 -0700

    added conditional rendering on buttons

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,19 @@ import styles from './ui/ColorBackground.module.css';
 import ButtonOnClick from './ui/ButtonOnClick'
 
 export default function Home() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code')!;
+  let onclickFunc = authorize;
+  let btnLabel = 'Login With Spotify'
+  if (code !== null) {
+    onclickFunc = getToken;
+    btnLabel = 'Get Token'
+  }
   return (
     <div className={styles.colorBackground}>
     <div className={styles.wavyLine}></div>
     <div className={styles.buttonContainer}>
-      <ButtonOnClick onClick={authorize} label='Login With Spotify'/>
-      <ButtonOnClick onClick={getToken} label='Get Token'/>
+      <ButtonOnClick onClick={onclickFunc} label={btnLabel}/>
     </div>
   </div>
   )
