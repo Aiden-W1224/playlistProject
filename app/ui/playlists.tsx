85ca5981613b403b2c4ac443f579ab287cd73990
Amka Tsendjargal <amka150@gmail.com>
2023-11-14 13:24:46 -0700

    passing tracks endpoint to Songs component

--- app/ui/playlists.tsx
+++ app/ui/playlists.tsx
@@ -3,6 +3,7 @@ import { fetchPlaylists, fetchTracks } from '../lib/fetchPlaylists';
 
 export default function Playlists(props: { accessToken: string }) {
   const [result, setResult] = useState<any>(null);
+  const [endpoint, setEndpoint] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,15 +23,30 @@ export default function Playlists(props: { accessToken: string }) {
     return <p>Loading...</p>; // or handle the loading state in a different way
   }
 
+  const handleClick = (endpointHref: string) => {
+    setEndpoint(endpointHref);
+  }
+
   console.log(result)
 
   // Render the items
   return (
-    <div className="flex flex-col space-y-4">
-      {result.items.map((item: any) => (
-        <li className='list-none' key={item.id}><StylePlaylist item = {item}/></li>
-      ))}
+    <div className="flex max-h-screen w-full px-10 py-32">
+        <div className="grid h-screen-15 flex-grow card bg-base-300 rounded place-items-center border-solid border-2 overflow-auto">
+          <div className="flex flex-col space-y-4">
+            {result.items.map((item: any) => (
+              <li className='list-none' key={item.id} onClick={() => handleClick(item.tracks.href)}><StylePlaylist item = {item}/></li>
+            ))}
+          </div>
+        </div>
+        <div className="divider lg: divider-neutral-horizontal">
+            
+        </div>
+        <div className="grid flex-grow card bg-base-300 rounded border-solid border-2 place-items-center overflow-auto">
+          <Songs endpoint={endpoint} accessToken={props.accessToken}/>
+        </div>
     </div>
+    
   );
 }
 
@@ -55,27 +71,29 @@ export function StylePlaylist(props: {item: any}) {
     );
 }
 
-// export function Songs(endpoint: string, accessToken: string) {
-//   const [result, setResult] = useState<any>(null);
-//   //const [select, setSelect] = useState<string>("");
+export function Songs(props: {endpoint: string, accessToken: string}) {
+  const [result, setResult] = useState<any>(null);
+  //const [select, setSelect] = useState<string>("");
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const tracksResult = await fetchTracks(endpoint, accessToken);
-//         setResult(tracksResult);
-//       } catch (error) {
-//         console.error('Error fetching tracks:', error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tracksResult = await fetchTracks(props.endpoint, props.accessToken);
+        setResult(tracksResult);
+      } catch (error) {
+        console.error('Error fetching tracks:', error);
+      }
+    };
 
-//     fetchData();
-//   }, [accessToken]);
+    fetchData();
+  }, [props.accessToken]);
 
-//   // Check if result is truthy and has items property
-//   if (!result || !result.items) {
-//     return <p>Loading...</p>; // or handle the loading state in a different way
-//   }
-// }
+  console.log(props.endpoint);
+
+  // Check if result is truthy and has items property
+  if (!result || !result.items) {
+    return <p>Loading...</p>; // or handle the loading state in a different way
+  }
+}
 
 
