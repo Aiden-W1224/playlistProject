2b49d41728a9225ef0f177045fc88a52176d46f9
Aiden Wong <aidenw@Aidens-MacBook-Pro.local>
2023-11-14 13:40:51 -0700

    cleaning up

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,16 +6,6 @@ import Playlists from "../ui/playlists";
 export default function Page() {
     const accessToken = localStorage.getItem('access_token')!;
     return(
-        //<Playlists accessToken={accessToken} />
-        <div className="flex max-h-screen w-full px-10 py-32">
-            <div className="grid h-screen-15 flex-grow card bg-base-300 rounded place-items-center border-solid border-2 overflow-auto">
-                <Playlists accessToken={accessToken}/>
-            </div>
-            <div className="divider lg: divider-neutral-horizontal">
-                
-            </div>
-            <div className="grid flex-grow card bg-base-300 rounded border-solid border-2 place-items-center overflow-auto">
-            </div>
-        </div>
+        <Playlists accessToken={accessToken} />
     )
 }
