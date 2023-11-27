d9e2443f5cfa4a861e704e61a303f7dbd05650b1
Amka Tsendjargal <amka150@gmail.com>
2023-11-27 14:56:23 -0700

    styling/ each songs are contained in a card/ added playlist public private indicator

--- app/ui/playlists.tsx
+++ app/ui/playlists.tsx
@@ -34,22 +34,22 @@ export default function Playlists(props: { accessToken: string }) {
 
   // Render the items
   return (
-    <div className="flex max-h-screen w-full px-10 py-32">
-        <div className="grid h-screen-15 flex-grow card bg-base-300 rounded place-items-center border-solid border-2 overflow-auto">
-          <div className="flex flex-col space-y-4">
+    <div className="flex flex-wrap">
+      <div className="w-full md:w-1/2">
+        <div className="max-h-[80vh] overflow-y-scroll mt-4">
+          <ul className="list-disc pl-4">
             {result.items.map((item: any) => (
-              <li className='list-none' key={item.id} onClick={() => handleClick(item)}><StylePlaylist item = {item}/></li>
+              <li className='list-none p-3' key={item.id} onClick={() => handleClick(item)}><StylePlaylist item = {item}/></li>
             ))}
-          </div>
+          </ul>
         </div>
-        <div className="divider lg: divider-neutral-horizontal">
-            
-        </div>
-        <div className="grid flex-grow card bg-base-300 rounded border-solid border-2 place-items-center overflow-auto">
+      </div>
+      <div className="w-full md:w-1/2">
+        <div className="max-h-[80vh] overflow-y-scroll mt-4">  
           {endpoint !== "" ? <Songs endpoint={endpoint} accessToken={props.accessToken}/> : null}
-          {/* <Songs endpoint={endpoint} accessToken={props.accessToken}/> */}
         </div>
-        {endpoint !== "" ? <TransferButton endpoint={endpoint} accessToken={props.accessToken} selectedPlaylist={selectedPlaylist}/> : null}
+      </div>
+      {endpoint !== "" ? <TransferButton endpoint={endpoint} accessToken={props.accessToken} selectedPlaylist={selectedPlaylist}/> : null}
     </div>
     
   );
@@ -62,14 +62,15 @@ export function StylePlaylist(props: {item: any}) {
     }
     return(
     <div className="flex flex-col max-w-full">
-      <div className="flex flex-col p-8 bg-white shadow-md hover:shadow-lg rounded-2xl">
+      <div className="flex flex-col p-8 bg-white shadow-md hover:bg-violet-400 transition ease-in rounded-2xl">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <img src={imageUrl} width='64px' height='64px'/>
             <div className="flex flex-col ml-3">
-              <p className="text-sm text-gray-600 leading-none mt-1">{props.item.name}</p>  
+              <p className="font-bold text-gray-600 leading-none mt-1">{props.item.name}</p>  
             </div>
           </div>
+          {props.item.public ? <img src='/public-icon.svg' alt='public' width='18px' height='18px'/> : <img src='/private-icon.svg' alt='private' width='18px' height='18px'/>}
         </div>
       </div>
     </div>
@@ -94,8 +95,6 @@ export function Songs(props: {endpoint: string, accessToken: string}) {
     fetchData();
   }, [props.endpoint]);
 
-  console.log(result)
-
   // Check if result is truthy and has items property
   if (!result || !result.items) {
     return <p>Loading...</p>; // or handle the loading state in a different way
@@ -103,7 +102,24 @@ export function Songs(props: {endpoint: string, accessToken: string}) {
   return(
     <div className="flex flex-col space-y-4">
       {result.items.map((item: any) => (
-        <li className='list-none'key={item.track.id}>{item.track.name}</li>
+        <li className='list-none' key={item.track.id}>
+          <div className="flex flex-col max-w-full">
+            <div className="flex flex-col p-8 bg-white shadow-md hover:bg-violet-400 transition ease-in rounded-2xl">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <div className="flex flex-col ml-3">
+                    <p className="font-bold text-gray-600 leading-none mt-1">{item.track.name}</p>
+                    {item.track.artists.map((artist: any) => (
+                      <p className="text-sm text-gray-600 leading-none mt-1">
+                        {artist.name}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
       ))}
     </div>    
   );
@@ -134,8 +150,6 @@ function TransferButton(props: {endpoint: string, accessToken: string, selectedP
       setTrackArray([...result.items])     
     }
   }, [result])
-  console.log(result)
-  console.log(trackArray)
 
   if(trackArray.length > 0) {
     //console.log(trackArray[0].track.name);
