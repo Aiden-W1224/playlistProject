12539d2bd48e65f2f6c1585160a350e705f702db
Amka Tsendjargal <amka150@gmail.com>
2023-12-01 21:34:32 -0700

    fixed a cors issue

--- app/lib/ytmusic/auth.ts
+++ app/lib/ytmusic/auth.ts
@@ -1,6 +1,6 @@
 export const getYouTubeToken = async () => {
 
-    const body = await fetch('http://127.0.0.1:5000/get-token');
+    const body = await fetch('http://127.0.0.1:5000/get-token', {mode:'cors'});
     const response = await body.json();
 
     localStorage.setItem("ytmusicToken", response.access_token)
