bf36cbabc5ba290961f782b4635fcdb3b3ffe823
Amka Tsendjargal <amka150@gmail.com>
2023-12-04 13:22:49 -0700

    added search feature for playlists and selected playlists is highlighted

--- app/ui/playlists.tsx
+++ app/ui/playlists.tsx
@@ -8,6 +8,7 @@ export default function Playlists(props: { accessToken: string }) {
   const [result, setResult] = useState<any>(null);
   const [endpoint, setEndpoint] = useState<string>("");
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+  const [filterText, setFilterText] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,16 +43,17 @@ export default function Playlists(props: { accessToken: string }) {
   return (
     <div className="flex flex-wrap">
       <div className="w-full md:w-1/2">
+        <SearchBar filterText={filterText} onFilterChange={setFilterText}/>
         <div className="max-h-[80vh] overflow-y-scroll mt-4">
           <ul className="list-disc pl-4">
-            {result.items.map((item: any) => (
-              <li className='list-none p-3 active:border-red-600' key={item.id} ref={refs[item.id]} onClick={() => handleClick(item)}><StylePlaylist item = {item}/></li>
+            {result.items.filter((item : any) => item.name.toLowerCase().includes(filterText.toLowerCase())).map((item: any) => (
+              <li className='list-none p-3' key={item.id} ref={refs[item.id]} onClick={() => handleClick(item)}><StylePlaylist item = {item} isActive = {item === selectedPlaylist}/></li>
             ))}
           </ul>
         </div>
       </div>
       <div className="w-full md:w-1/2">
-        <div className="max-h-[80vh] overflow-y-scroll mt-4">  
+        <div className="max-h-[80vh] overflow-y-scroll mt-[60px]">  
           {endpoint !== "" ? <Songs endpoint={endpoint} accessToken={props.accessToken}/> : null}
         </div>
       </div>
@@ -61,14 +63,14 @@ export default function Playlists(props: { accessToken: string }) {
   );
 }
 
-export function StylePlaylist(props: {item: any}) {
+export function StylePlaylist(props: {item: any, isActive: boolean}) {
     let imageUrl = '/defaultPlaylistImage.png';
     if (props.item.images.length > 0) {
       imageUrl = props.item.images[0].url;
     }
     return(
     <div className="flex flex-col max-w-full">
-      <div className="flex flex-col p-8 bg-white shadow-md hover:bg-violet-400 transition ease-in rounded-2xl">
+      <div className={`flex flex-col p-8 shadow-md hover:bg-violet-400 transition ease-in ${props.isActive ? 'bg-violet-500' : " bg-white"} rounded-2xl`}>
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <img src={imageUrl} width='64px' height='64px'/>
@@ -177,13 +179,27 @@ function TransferButton(props: {endpoint: string, accessToken: string, selectedP
 
   return (
     <div className='flex w-full items-center'>
-      <button className='bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 mx-7 my-4 rounded focus:ring transform transition duration-300 ease-in-out' onClick={handleClick}>Transfer</button>
+      <button className='bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 mx-7 my-2 rounded focus:ring transform transition duration-300 ease-in-out' onClick={handleClick}>Transfer</button>
       {loading ? <Spinner /> : null}
     </div>
     
   )
-  
+}
 
+function SearchBar(props : {filterText : string, onFilterChange : any}) {
+  return (
+    <div className="w-full flex justify-center pe-3 ps-7">
+      <div className="relative w-full">
+        <input type="text" className="w-full backdrop-blur-sm bg-white/20 py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-violet-300 transition-colors duration-300"
+           value={props.filterText} placeholder="Search..." onChange={(e) => props.onFilterChange(e.target.value)}/>
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 function Spinner() {
